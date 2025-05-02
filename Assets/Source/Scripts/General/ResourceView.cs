89f9bc3c9b60fc70ced7f625f83548bcc44b61a1
Millon13 <lek13millon@gmail.com>
2025-05-02 13:45:56 +0300

    3
    Поменяла Logics(Vievs) чтоб были изображения, а не скролы

--- Assets/Source/Scripts/General/ResourceView.cs
+++ Assets/Source/Scripts/General/ResourceView.cs
@@ -3,25 +3,35 @@ using UnityEngine.UI;
 
 public class ResourceView : MonoBehaviour
 {
-    [SerializeField] private Slider _slider;
+    [SerializeField] private Image _image; 
     private IResource _resource;
 
     private void OnDestroy()
     {
-        _resource.ResourcesAmountChanged -= OnResourceChanged;
+        if (_resource != null)
+        {
+            _resource.ResourcesAmountChanged -= OnResourceChanged;
+        }
     }
 
     public void Init(IResource resource)
     {
         _resource = resource;
-        _slider.maxValue = _resource.Maximum;
-        _slider.value = _resource.Amount;
+        UpdateImageFill(); 
 
         _resource.ResourcesAmountChanged += OnResourceChanged;
     }
 
     private void OnResourceChanged()
     {
-        _slider.value = _resource.Amount;
+        UpdateImageFill(); 
+    }
+
+    private void UpdateImageFill()
+    {
+        
+        float fillAmount = _resource.Amount / _resource.Maximum;
+        _image.fillAmount = fillAmount; 
     }
 }
+
