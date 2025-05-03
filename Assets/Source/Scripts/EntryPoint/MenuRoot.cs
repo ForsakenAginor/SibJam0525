ae547bf17d421e5257d5e2e4ef3c4ea80439f9ba
ForsakenAginor <128065996+ForsakenAginor@users.noreply.github.com>
2025-05-03 14:41:54 +0400

    menu scene

--- Assets/Source/Scripts/EntryPoint/MenuRoot.cs
+++ Assets/Source/Scripts/EntryPoint/MenuRoot.cs
@@ -1,15 +1,22 @@
 using Assets.Scripts.General;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuRoot : MonoBehaviour
 {
     [SerializeField] private Button _playButton;
+    [SerializeField] private Button _authorsButton;
+    [SerializeField] private Button _closeAuthorsButton;
+    [SerializeField] private SwitchableElement _buttonsPanel;
+    [SerializeField] private SwitchableElement _authorsPanel;
 
     private void Start()
     {
         Time.timeScale = 0f;
         _playButton.onClick.AddListener(OnPlayButtonClick);
+        _authorsButton.onClick.AddListener(OnAuthorsOpen);
+        _closeAuthorsButton.onClick.AddListener(OnCloseAuthorsButton);
 
         Time.timeScale = 1f;
         SceneChangerSingleton.Instance.FadeOut();
@@ -18,6 +25,20 @@ public class MenuRoot : MonoBehaviour
     private void OnDestroy()
     {
         _playButton.onClick.RemoveListener(OnPlayButtonClick);
+        _authorsButton.onClick.RemoveListener(OnAuthorsOpen);
+        _closeAuthorsButton.onClick.RemoveListener(OnCloseAuthorsButton);
+    }
+
+    private void OnAuthorsOpen()
+    {
+        _authorsPanel.Enable();
+        _buttonsPanel.Disable();
+    }
+
+    private void OnCloseAuthorsButton()
+    {
+        _authorsPanel.Disable();
+        _buttonsPanel.Enable();
     }
 
     private void OnPlayButtonClick()
